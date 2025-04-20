3d3e3af63232e36868b0ca7464f53ea1112d794a
Rushi <rushi.jetalpuria@paramount.com>
2025-04-20 14:43:28 -0700

    Adds implementation for chat memory

--- src/main/java/com/spring/ai/tutorial/controller/GenAiController.java
+++ src/main/java/com/spring/ai/tutorial/controller/GenAiController.java
@@ -1,7 +1,10 @@
 package com.spring.ai.tutorial.controller;
 
 import com.spring.ai.tutorial.service.ChatService;
+import java.util.List;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.messages.Message;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,16 +17,31 @@ import reactor.core.publisher.Flux;
 @RequiredArgsConstructor
 public class GenAiController {
   private final ChatService chatService;
+  private static final String CHAT_ID_HEADER = "chat-id";
 
   @GetMapping("/chat")
-  public ResponseEntity<String> getChatResponse(@RequestParam String prompt) {
-    String response = chatService.getResponse(prompt);
-    return ResponseEntity.ok(response);
+  public ResponseEntity<String> getChatResponse(
+      @RequestParam String prompt, @RequestParam(required = false) UUID chatId) {
+    if (chatId == null) {
+      chatId = UUID.randomUUID();
+    }
+    String response = chatService.getResponse(prompt, chatId);
+    return ResponseEntity.ok().header(CHAT_ID_HEADER, chatId.toString()).body(response);
   }
 
   @GetMapping("/streaming-chat")
-  public ResponseEntity<Flux<String>> getStreamingResponse(@RequestParam String prompt) {
-    Flux<String> response = chatService.getStreamingResponse(prompt);
-    return ResponseEntity.ok(response);
+  public ResponseEntity<Flux<String>> getStreamingResponse(
+      @RequestParam String prompt, @RequestParam(required = false) UUID chatId) {
+    if (chatId == null) {
+      chatId = UUID.randomUUID();
+    }
+    Flux<String> response = chatService.getStreamingResponse(prompt, chatId);
+    return ResponseEntity.ok().header(CHAT_ID_HEADER, chatId.toString()).body(response);
+  }
+
+  @GetMapping("/chat-history")
+  public ResponseEntity<List<Message>> getChatHistory(@RequestParam(required = true) UUID chatId) {
+    List<Message> chatHistory = chatService.getChatHistory(chatId);
+    return ResponseEntity.ok().body(chatHistory);
   }
 }
--- src/main/java/com/spring/ai/tutorial/service/impl/ChatServiceImpl.java
+++ src/main/java/com/spring/ai/tutorial/service/impl/ChatServiceImpl.java
@@ -1,12 +1,22 @@
 package com.spring.ai.tutorial.service.impl;
 
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_RETRIEVE_SIZE_KEY;
+
 import com.spring.ai.tutorial.service.ChatService;
+import jakarta.annotation.PostConstruct;
+import java.util.List;
+import java.util.UUID;
 import java.util.function.Predicate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.ai.chat.model.StreamingChatModel;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,7 +28,8 @@ import reactor.core.publisher.Flux;
 @RequiredArgsConstructor
 public class ChatServiceImpl implements ChatService {
   private final ChatModel chatModel;
-  private final StreamingChatModel streamingChatModel;
+  private ChatMemory chatMemory;
+  private ChatClient chatClient;
 
   @Value("${com.ai.springAiTutorial.model}")
   private String modelName;
@@ -29,22 +40,43 @@ public class ChatServiceImpl implements ChatService {
   @Value("${com.ai.springAiTutorial.temperature}")
   private Double temperature;
 
+  @Value("${com.ai.springAiTutorial.chatMemorySize}")
+  private Integer chatMemorySize;
+
   private final Predicate<ChatResponse> isResponseValid =
       response ->
           response != null
               && response.getResult() != null
               && response.getResult().getOutput() != null;
 
-  public String getResponse(String msg) {
-    OpenAiChatOptions openAiChatOptions =
-        OpenAiChatOptions.builder().model(modelName).topP(topP).temperature(temperature).build();
-
-    Prompt prompt = new Prompt(msg, openAiChatOptions);
-    ChatResponse response = chatModel.call(prompt);
-    return response.getResult().getOutput().getText();
+  @PostConstruct
+  void init() {
+    chatMemory = new InMemoryChatMemory();
+    chatClient =
+        ChatClient.builder(chatModel)
+            .defaultAdvisors(new MessageChatMemoryAdvisor(chatMemory))
+            .build();
   }
 
-  public Flux<String> getStreamingResponse(String msg) {
+  public String getResponse(String msg, UUID conversationId) {
+    OpenAiChatOptions openAiChatOptions =
+        OpenAiChatOptions.builder().model(modelName).topP(topP).temperature(temperature).build();
+    Prompt prompt = new Prompt(msg, openAiChatOptions);
+
+    ChatResponse response =
+        chatClient
+            .prompt(prompt)
+            .advisors(
+                advisor ->
+                    advisor
+                        .param(CHAT_MEMORY_CONVERSATION_ID_KEY, conversationId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, chatMemorySize))
+            .call()
+            .chatResponse();
+    return isResponseValid.test(response) ? response.getResult().getOutput().getText() : null;
+  }
+
+  public Flux<String> getStreamingResponse(String msg, UUID conversationId) {
     OpenAiChatOptions openAiChatOptions =
         OpenAiChatOptions.builder()
             .model(modelName)
@@ -53,15 +85,22 @@ public class ChatServiceImpl implements ChatService {
             .streamUsage(true)
             .build();
 
-    Prompt prompt = new Prompt(msg, openAiChatOptions);
-    return streamingChatModel.stream(prompt)
-        .mapNotNull(
-            response -> {
-              if (isResponseValid.test(response)) {
-                return response.getResult().getOutput().getText();
-              } else {
-                return null;
-              }
-            });
+    Flux<ChatResponse> response =
+        chatClient
+            .prompt(new Prompt(msg, openAiChatOptions))
+            .advisors(
+                advisor ->
+                    advisor
+                        .param(CHAT_MEMORY_CONVERSATION_ID_KEY, conversationId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, chatMemorySize))
+            .stream()
+            .chatResponse();
+
+    return response.mapNotNull(
+        resp -> isResponseValid.test(resp) ? resp.getResult().getOutput().getText() : null);
+  }
+
+  public List<Message> getChatHistory(UUID conversationId) {
+    return chatMemory.get(conversationId.toString(), 100);
   }
 }
